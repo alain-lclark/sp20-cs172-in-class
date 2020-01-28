cd90f6e4d6780f3acacafa303d1392a81323a88a
Alain Kägi <alaink@lclark.edu>
2020-01-27 17:36:13 -0800

    Change some while loops into for loops.

--- src/SpeedReader.java
+++ src/SpeedReader.java
@@ -1,15 +1,14 @@
 public class SpeedReader {
     public static void main(String[] args) {
-        while (true) {
+        for (;;) {
             In file = new In("speed.txt");
             while (!file.isEmpty()) {
                 String s = file.readLine();
                 int i = 0;
                 while (i < s.length()) {
                     String w = "";
-                    while (i < s.length() && s.charAt(i) != ' ') {
+                    for (; i < s.length() && s.charAt(i) != ' '; i++) {
                         w = w + s.charAt(i);
-                        i++;
                     }
                     i++;
                     StdDraw.clear();
