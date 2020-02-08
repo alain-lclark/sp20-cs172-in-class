2bae8a2c05e945033544a78f49b1e337fd9837a0
Alain Kägi <alaink@lclark.edu>
2020-02-07 17:19:10 -0800

    Added hasWon() and autoClear() methods.

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -3,10 +3,11 @@ public class Minesweeper {
         StdOut.println("welcome to MineSweeper");
         int size=10;
         boolean minefield[][]=new boolean[size][size];
+        boolean revealed[][] = new boolean[size][size];
         int numMines=5;
         initMinefield(minefield,numMines);
         drawMinefield(minefield);
-        while(!hasWon(minefield)){
+        while(!hasWon(minefield, revealed)){
             processClick();
             drawMinefield(minefield);
         }
@@ -16,9 +17,29 @@ public class Minesweeper {
 
     }
 
-    private static boolean hasWon(boolean[][] minefield) {
-
+    public static boolean hasWon(boolean[][] minefield, boolean[][] revealed) {
+        for(int i = 0; i < minefield.length; i++) {
+            for(int j = 0; j < minefield.length; j++) {
+                if(!(revealed[i][j] || minefield[i][j])) {
+                    return false;
+                }
+            }
+        }
         return true;
+    }
+    public static void autoClear(boolean[][] minefield, boolean[][] revealed, int x, int y) {
+        if(!revealed[x][y]) {
+            revealed[x][y] = true;
+            if (countNeighboringMines(minefield, x, y) == 0) {
+                for (int i = x - 1; i <= x + 1; i++) {
+                    for (int j = y - 1; j <= y + 1; j++) {
+                        if(i >= 0 && i < minefield.length && j >= 0 && j < minefield.length) {
+                            autoClear(minefield, revealed, i, j);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private static void drawMinefield(boolean[][] minefield) {
--- test/MinesweeperTest.java
+++ test/MinesweeperTest.java
@@ -10,4 +10,35 @@ class MinesweeperTest {
         assertEquals(0, Minesweeper.countNeighboringMines(minefield,2,2));
     }
 
+    @org.junit.jupiter.api.Test
+    void aWinner() {
+        boolean[][]minefield= new boolean[3][3];
+        minefield[1][0]=true;
+        boolean[][] revealed = new boolean[3][3];
+        revealed[0][0] = true;
+        revealed[0][1] = true;
+        revealed[0][2] = true;
+        revealed[1][1] = true;
+        revealed[1][2] = true;
+        revealed[2][0] = true;
+        revealed[2][1] = true;
+        revealed[2][2] = true;
+        assertTrue(Minesweeper.hasWon(minefield, revealed));
+    }
+
+    @org.junit.jupiter.api.Test
+    void aLoser() {
+        boolean[][]minefield= new boolean[3][3];
+        minefield[1][0]=true;
+        boolean[][] revealed = new boolean[3][3];
+        revealed[0][0] = true;
+        revealed[0][1] = true;
+        revealed[0][2] = true;
+        revealed[1][1] = true;
+        revealed[1][2] = true;
+        revealed[2][0] = true;
+        revealed[2][1] = true;
+        assertFalse(Minesweeper.hasWon(minefield, revealed));
+    }
+
 }
