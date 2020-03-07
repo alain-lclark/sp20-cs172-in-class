e10273ccdc600a924ea5baed2885903b8abc7498
Alain Kägi <alaink@lclark.edu>
2020-03-06 16:21:53 -0800

    Implement the functions in Node.java using crowdsourcing and recursion.

--- src/Node.java
+++ src/Node.java
@@ -8,39 +8,49 @@ public class Node {
     Node next;
 
     /**
-     * Return the length of list start at n.
+     * Return the length of list.
      * @param list  a list
-     * @return the number of nodes in the list n
+     * @return the number of nodes in the list
      */
     static int length(Node list) {
         if (list == null) {
             return 0;
         }
-        int length = 1;
-        Node here = list;
-        while (here.next != null) {
-            ++length;
-            here = here.next;
-        }
-        return length;
+        return 1 + length(list.next);
     }
 
     /**
-     * Return the sum of the elements in the list n.
-     * @param n  a list of numbers
-     * @return the sum of the elements in the list n
+     * Return the sum of the elements in the list.
+     * @param list  a list of numbers
+     * @return the sum of the elements in the list
      */
-    static int sum(Node n) {
-        return 0;
+    static int sum(Node list) {
+        if (list == null) {
+            return 0;
+        }
+        return list.item + sum(list.next);
     }
 
     /**
      * Return the largest element in the list n.
-     * @param n  a list of numbers
-     * @return the largest element in the list n
+     *
+     * Assumption n has at least one element.
+     *
+     * @param list  a list of numbers
+     * @return the largest element in the list
      */
-    static int max(Node n) {
-        return 0;
+    static int max(Node list) {
+        if (list.next == null) {
+            // Singleton list case.
+            return list.item;
+        }
+        // More than one element in the list.
+        if (list.item > max(list.next)) {
+            return list.item;
+        }
+        else {
+            return max(list.next);
+        }
     }
 
     /**
@@ -54,6 +64,15 @@ public class Node {
      * @return true if the lists n and m contains the same elements.
      */
     static boolean equals(Node n, Node m) {
+        if (length(n) != length(m)) {
+            return false;
+        }
+        if (length(n) == 0) {
+            return true;
+        }
+        if (n.item == m.item) {
+            return equals(n.next, m.next);
+        }
         return false;
     }
 
